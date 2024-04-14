9cf7f17053fc3fdb9c4f1b5e9cd87c912aff4e2f
Nikita Nikandrov <nnikandroff@yandex.ru>
2024-04-14 16:08:41 +0300

    Initial Commit

---
+++ NameInfoApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NameInfoApp
+//
+//  Created by Nikita Nikandrov on 14.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
